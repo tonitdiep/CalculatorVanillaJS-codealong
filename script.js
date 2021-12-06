f33442427c97aecbd5e875a078669a00dc4ac844
tonitdiep <adiep123@gmail.com>
2021-12-06 14:34:42 -0500

    created JS buttons. how to store info when using button?  created a Calculator class with functions to clear, delete, appendNumber, compute and updateDisplay

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 console.log("first JS program this week");
+//create a calcutaor class
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear() {
 
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+           
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay() {
+
+    }
+    
+
+}
 //select all different #, operations, AC, DEL and other misc buttons
 //add classes to HTML so use data attributes to not mixed w/ HTML
 // between stylizng classes and JS
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const equalsButton = document.querySelector('[data-equals]')
+
