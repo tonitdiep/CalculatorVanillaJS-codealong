be316ebd8542d4e070c56a7395b6b7bdd5bd857e
tonitdiep <adiep123@gmail.com>
2021-12-06 13:51:30 -0500

    update index.html with concise data set naming convention separate from styzling classes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("first JS program this week");
+
+//select all different #, operations, AC, DEL and other misc buttons
+//add classes to HTML so use data attributes to not mixed w/ HTML
+// between stylizng classes and JS
