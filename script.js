e7c5993b615a3c26e84a99b32fe4bea40a9982fb
tonitdiep <adiep123@gmail.com>
2021-12-26 04:31:39 -0500

    updated addEvenntListener's with equalsButton, allClearButton and deleteButton. updated class Calculator's delete()

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -103,5 +103,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button =>{
+    calculator.delete()
     calculator.updateDisplay()
 })
